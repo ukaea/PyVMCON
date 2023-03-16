3fd5263504d86bcad76ce99f3d0707d451e7cb88
Timothy Nunn <timothy.nunn@ukaea.uk>
2023-03-16 16:16:56 +0000

    Use cvxpy constant for solver selection

--- src/pyvmcon/vmcon.py
+++ src/pyvmcon/vmcon.py
@@ -232,7 +232,7 @@ def solve_qsp(
         constraints.append((result.deq @ delta) + result.eq == 0)
 
     qsp = cp.Problem(problem_statement, constraints or None)
-    qsp.solve(verbose=False, solver="OSQP", eps_rel=tolerance)
+    qsp.solve(verbose=False, solver=cp.OSQP, eps_rel=tolerance)
 
     if delta.value is None:
         raise _QspSolveException(f"QSP failed to solve: {qsp.status}")
