6ebb28baa180a2b52af7c625d0a9fad40d396de7
Timothy <75321887+timothy-nunn@users.noreply.github.com>
2023-08-16 15:23:54 +0100

    Upversion to 2.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ copyright = "2023, UK Atomic Energy Authority"
 author = "Timothy Nunn"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "2.1.0"
 
 
 # -- General configuration ---------------------------------------------------
