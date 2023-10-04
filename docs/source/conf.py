9272ec74ac8ce95c5c71caa4a16002ccdc624ffd
Timothy Nunn <timothy.nunn@ukaea.uk>
2023-10-04 16:30:45 +0100

    Upversion to 2.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ copyright = "2023, UK Atomic Energy Authority"
 author = "Timothy Nunn"
 
 # The full version, including alpha/beta/rc tags
-release = "2.1.0"
+release = "2.1.1"
 
 
 # -- General configuration ---------------------------------------------------
