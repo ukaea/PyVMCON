eb0d67367b8256d34b0c8da9815b4b9e9157f687
Timothy Nunn <timothy.nunn@ukaea.uk>
2024-05-10 13:12:14 +0100

    Update copyright information in preperation for 2.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -- Project information -----------------------------------------------------
 
 project = "PyVMCON"
-copyright = "2023, UK Atomic Energy Authority"
+copyright = "2023-2024, UK Atomic Energy Authority"
 author = "Timothy Nunn"
 
 # The full version, including alpha/beta/rc tags
