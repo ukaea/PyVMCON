348605d1a8d14e7f58ead3c7210e27f4487006a0
Timothy <75321887+timothy-nunn@users.noreply.github.com>
2024-06-19 15:08:19 +0100

    Update cvxpy version with numpy major release (#15)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ copyright = "2023-2024, UK Atomic Energy Authority"
 author = "Timothy Nunn"
 
 # The full version, including alpha/beta/rc tags
-release = "2.2.0"
+release = "2.2.1"
 
 
 # -- General configuration ---------------------------------------------------
